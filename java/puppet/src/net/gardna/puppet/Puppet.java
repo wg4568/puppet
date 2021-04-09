e06f46306c225e30dc2e6ed9487045046420d7a3
William Gardner <william@gardna.net>
2021-04-08 18:48:31 -0700

    added plugin template

---
+++ java/puppet/src/net/gardna/puppet/Puppet.java
@@ -0,0 +1,22 @@
+package net.gardna.puppet;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+class MyCommandExecutor implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("You ran /mycommand");
+
+        return true;
+    }
+}
+
+public class Puppet extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getCommand("mycommand").setExecutor(new MyCommandExecutor());
+    }
+}
